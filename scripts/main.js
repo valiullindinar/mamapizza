0e83eacb46d6e9d8814630ba146b39609c5a3ae7
Dinar Valiullin <valiullindinar@gmail.com>
2017-05-31 22:15:53 +0300

    Added animation logo

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,58 @@
+ document.addEventListener('DOMContentLoaded', function() {
+
+        var svg = new Walkway('#triangle');
+
+        svg.draw(function() {
+          console.log('Finished triangle!');
+        });
+
+        var svg2 = new Walkway({
+          selector: '#diamond',
+          duration: 2500,
+          easing: 'linear'
+        }).draw(function() {
+          console.dir('Finished diamond!');
+        });
+
+        var svg3 = new Walkway({
+          selector: '#tester',
+          duration: 5000,
+          easing: 'linear'
+        });
+
+        svg3.draw(function() {
+          console.log('Finished test shape!');
+        });
+
+        var svg4 = new Walkway({
+          selector: '#ps4',
+          duration: 3500
+        });
+
+        svg4.draw();
+
+        var svg5 = new Walkway({
+          selector: '#polyline',
+          duration: 3000
+        });
+
+        svg5.draw(function() {
+          console.log('Finished polyline!');
+        });
+
+        var svg6 = new Walkway({
+          selector: '#customEasing',
+          duration: 3000,
+          easing: function(t) {
+            return t * t * t;
+          }
+        }).draw();
+
+        document.addEventListener('dblclick', function() {
+          svg.redraw();
+          svg4.redraw();
+        }, false);
+      });
 $(document).ready(function() {
-	
+
 })
